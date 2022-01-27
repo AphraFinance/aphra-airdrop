6fe3464554c7bebd0d6f8e90099545014d3c8f3d
Andrew Redden <opensource@andrewredden.com>
2022-01-27 14:47:00 -0400

    aphra launch prep

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,20 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  redirects: [
+    {
+      source: "http://drop.aphra.finance/",
+      destination: "https://drop.aphra.finance",
+      type: 301,
+    },
+  ],
   reactStrictMode: true,
   exportPathMap: async function (
-      defaultPathMap,
-      { dev, dir, outDir, distDir, buildId }
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
   ) {
     return {
-      '/': { page: '/index' },
-      '/claim': { page: '/claim' },
-
-    }
+      "/": { page: "/index" },
+      "/claim": { page: "/claim" },
+    };
   },
 };
